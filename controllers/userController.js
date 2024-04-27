f49a78fbce5547608ee55153d4889355d07235e2
TranHuy98 <huy8663@gmail.com>
2024-04-27 11:30:00 +0700

    update 1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import UserModel from "../models/users.js";
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
 import { generateToken } from "../middlewares/token.js";
+import bcrypt from 'bcryptjs';
 
 const userController = {
     //sign up
